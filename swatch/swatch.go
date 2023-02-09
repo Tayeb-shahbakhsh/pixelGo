5dc822f59fd129eb0fbff6ec4480dc6fef6b4dc5
Tayeb-shahbakhsh <shahbakhshtayyeb@gmail.com>
2023-02-09 13:26:58 +0330

    3 new files added to swatch pakage

---
+++ swatch/swatch.go
@@ -0,0 +1,31 @@
+package swatch
+
+import (
+	"fyne.io/fyne/v2/widget"
+	"image/color"
+	"tayebshahbakhsh/pixel/apptype"
+)
+
+type Swatch struct {
+	widget.BaseWidget
+	Selected     bool
+	Color        color.Color
+	SwatchIndex  int
+	clickHandler func(s *Swatch)
+}
+
+func (s *Swatch) SetColor(c color.Color) {
+	defer s.Refresh()
+	s.Color = c
+}
+
+func NewSwatch(state *apptype.State, color color.Color, swatchIndex int, clickHandler func(s *Swatch)) *Swatch {
+	swatch := &Swatch{
+		Selected:     false,
+		Color:        color,
+		clickHandler: clickHandler,
+		SwatchIndex:  swatchIndex,
+	}
+	swatch.ExtendBaseWidget(swatch)
+	return swatch
+}
